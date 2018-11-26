b659a9db69f77704dd65a15b65c384295b9aecc9
Daybrush <younkue.choi@navercorp.com>
2018-11-26 19:03:31 +0900

    fix(persist): fix babel config

--- karma.conf.js
+++ karma.conf.js
@@ -1,66 +1,81 @@
-module.exports = function(config) {
-  var karmaConfig = {
-    frameworks: ["mocha", "chai", "sinon"],
+module.exports = config => {
+	const karmaConfig = {
+		frameworks: ["mocha", "chai", "sinon"],
 
-    // list of files / patterns to load in the browser
-    files: [
-      "./node_modules/lite-fixture/index.js",
-      "./test/unit/**/*.spec.js"
-    ],
+		// list of files / patterns to load in the browser
+		files: [
+			"./node_modules/lite-fixture/index.js",
+			"./test/unit/**/*.spec.js",
+		],
 
-    client: {
-      mocha: {
-        opts: "./mocha.opts" 
-      }
-    },
+		client: {
+			mocha: {
+				opts: "./mocha.opts",
+			},
+		},
 
-    webpack: {
-      devtool: "inline-source-map",
-      module: {
-          rules: [
-              {
-                  test: /\.js$/,
-                  exclude: /node_modules/,
-                  loader: "babel-loader",
-              }
-          ]
-      }
-    },
+		webpack: {
+			devtool: "inline-source-map",
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						loader: "babel-loader",
+						query: {
+							"presets": [
+								[
+									"@babel/preset-env",
+									{
+										"loose": true,
+										"modules": "commonjs",
+									},
+								],
+							],
+							"plugins": [
+								"add-module-exports",
+								"transform-es3-property-literals",
+								"transform-es3-member-expression-literals",
+							],
+						},
+					},
+				],
+			},
+		},
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			"./test/**/*.spec.js": config.coverage ? ["webpack"] : ["webpack", "sourcemap"],
+		},
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      "./test/**/*.spec.js": config.coverage ? ["webpack"] : ["webpack", "sourcemap"]
-    },
+		browsers: [],
+		customLaunchers: {
+			CustomChromeHeadless: {
+				base: "ChromeHeadless",
+				flags: ["--disable-gpu", "--no-sandbox"],
+			},
+		},
+		reporters: ["mocha"],
+		webpackMiddleware: {
+			noInfo: true,
+		},
+	};
 
-    browsers: [],
-    customLaunchers: {
-      CustomChromeHeadless: {
-        base: 'ChromeHeadless',
-        flags: ['--disable-gpu', '--no-sandbox']
-      }
-    },
-    reporters: ["mocha"],
-    webpackMiddleware: {
-        noInfo: true
-    }
-  };
-  
-  karmaConfig.browsers.push(config.chrome ? "Chrome" : "CustomChromeHeadless");
+	karmaConfig.browsers.push(config.chrome ? "Chrome" : "CustomChromeHeadless");
 
-  if(config.coverage) {
-    karmaConfig.reporters.push("coverage-istanbul");
-    karmaConfig.coverageIstanbulReporter = {
-      reports: ["text-summary", "html", "lcovonly"],
-      dir: "./coverage"
-    };
-    karmaConfig.webpack.module.rules.unshift({
-        test: /\.js$/,
-        exclude: /(node_modules|test)/,
-        loader: "istanbul-instrumenter-loader"
-    });
-    karmaConfig.singleRun = true;
-  }
+	if (config.coverage) {
+		karmaConfig.reporters.push("coverage-istanbul");
+		karmaConfig.coverageIstanbulReporter = {
+			reports: ["text-summary", "html", "lcovonly"],
+			dir: "./coverage",
+		};
+		karmaConfig.webpack.module.rules.unshift({
+			test: /\.js$/,
+			exclude: /(node_modules|test)/,
+			loader: "istanbul-instrumenter-loader",
+		});
+		karmaConfig.singleRun = true;
+	}
 
-  config.set(karmaConfig);
+	config.set(karmaConfig);
 };
