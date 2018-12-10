a5f4bbb54661f51f8ca89fff3bdcc2922ebb0ed0
qpitlove <qpitlove@gmail.com>
2018-12-10 12:47:47 +0900

    Hotfix/fix performace.navigation is existed (#31)
    * fix: Make sure  performance.navigation property exists.
    * fix: eslint error

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 import {navigator, parseFloat, performance} from "./browser";
 
 const userAgent = navigator ? navigator.userAgent : "";
-const TYPE_BACK_FORWARD = (performance && performance.navigation.TYPE_BACK_FORWARD) || 2;
+const TYPE_BACK_FORWARD = (performance && performance.navigation &&
+	performance.navigation.TYPE_BACK_FORWARD) || 2;
 
 const isNeeded = (function() {
 	const isIOS = (new RegExp("iPhone|iPad", "i")).test(userAgent);
@@ -22,7 +23,8 @@ const isNeeded = (function() {
 
 // In case of IE8, TYPE_BACK_FORWARD is undefined.
 function isBackForwardNavigated() {
-	return performance && (performance.navigation.type === TYPE_BACK_FORWARD);
+	return (performance && performance.navigation &&
+		performance.navigation.type) === TYPE_BACK_FORWARD;
 }
 
 export {
