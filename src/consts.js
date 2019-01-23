d81438d967a262d07f4474b4fde6175636281bfa
Daybrush <younkue.choi@navercorp.com>
2019-01-23 18:51:08 +0900

    feat(consts): add navgation's types and persist state

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,11 @@
-const CONST_PERSIST = "___persist___";
+import {performance} from "./browser";
 
-export default CONST_PERSIST;
+export const CONST_PERSIST = "___persist___";
+export const CONST_PERSIST_STATE = `state${CONST_PERSIST}`;
+export const CONST_DEPTHS = "depths";
+export const CONST_CURRENT_URL = "currentUrl";
+const navigation = performance && performance.navigation;
+
+export const TYPE_NAVIGATE = (navigation && navigation.TYPE_NAVIGATE) || 0;
+export const TYPE_RELOAD = (navigation && navigation.TYPE_RELOAD) || 1;
+export const TYPE_BACK_FORWARD = (navigation && navigation.TYPE_BACK_FORWARD) || 2;
