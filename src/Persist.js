93a47f6bb64ca5f905d31bbde5c8d5c8912ae714
Daybrush <younkue.choi@navercorp.com>
2019-01-23 19:09:01 +0900

    feat(Persist): add depth structure

--- src/Persist.js
+++ src/Persist.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-use-before-define */
 import * as StorageManager from "./storageManager";
-import {isNeeded, getUrlKey} from "./utils";
-import {console} from "./browser";
+import {isNeeded, getUrl, getKey, getNavigationType} from "./utils";
+import {console, window} from "./browser";
+import {TYPE_BACK_FORWARD, TYPE_NAVIGATE, CONST_PERSIST_STATE, CONST_DEPTHS, CONST_CURRENT_URL} from "./consts";
 
 function setRec(obj, path, value) {
 	let _obj = obj;
@@ -23,6 +25,63 @@ function setRec(obj, path, value) {
 	return _obj;
 }
 
+function updateDepth(currentUrl, type) {
+	const prevUrl = Persist.url;
+
+	if (prevUrl !== currentUrl) {
+		Persist.url = currentUrl;
+		const depths = StorageManager.getStateByKey(CONST_PERSIST_STATE, CONST_DEPTHS) || [];
+
+		if (type === TYPE_BACK_FORWARD) {
+			// Change current url only
+			const currentIndex = depths.indexOf(currentUrl);
+
+			if (~currentIndex) {
+				StorageManager.setStateByKey(CONST_PERSIST_STATE, CONST_CURRENT_URL, currentUrl);
+			}
+		} else {
+			const current = StorageManager.getStateByKey(CONST_PERSIST_STATE, CONST_CURRENT_URL);
+
+			StorageManager.reset(getKey(currentUrl), null);
+			if (type === TYPE_NAVIGATE) {
+				// Remove all url lists with higher index than current index
+				const prevIndex = depths.indexOf(current);
+				const removedList = depths.splice(prevIndex + 1, depths.length);
+
+				removedList.forEach(url => {
+					StorageManager.reset(getKey(url), null);
+				});
+				// If the type is NAVIGATE and there is information about current url, delete it.
+				const currentIndex = depths.indexOf(currentUrl);
+
+				~currentIndex && depths.splice(currentIndex, 1);
+				StorageManager.setStateByKey(CONST_PERSIST_STATE, CONST_DEPTHS, depths);
+			}
+		}
+	}
+}
+function addDepth(currentUrl) {
+	const prevUrl = Persist[CONST_CURRENT_URL];
+
+	if (currentUrl !== prevUrl) {
+		Persist[CONST_CURRENT_URL] = currentUrl;
+
+		const depths = StorageManager.getStateByKey(CONST_PERSIST_STATE, CONST_DEPTHS) || [];
+
+		if (depths.indexOf(currentUrl) < 0) {
+			depths.push(currentUrl);
+			StorageManager.setStateByKey(CONST_PERSIST_STATE, CONST_DEPTHS, depths);
+		}
+		StorageManager.setStateByKey(CONST_PERSIST_STATE, CONST_CURRENT_URL, currentUrl);
+	}
+}
+if (window.onpopstate) {
+	window.addEventListener("popstate", () => {
+		// popstate event occurs when backward or forward
+		updateDepth(getUrl(), TYPE_BACK_FORWARD);
+	});
+}
+
 /**
  * Get or store the current state of the web page using JSON.
  * @ko 웹 페이지의 현재 상태를 JSON 형식으로 저장하거나 읽는다.
@@ -33,28 +92,14 @@ function setRec(obj, path, value) {
 class Persist {
 	static VERSION = "#__VERSION__#";
 	static StorageManager = StorageManager;
+	static url = "";
+	static [CONST_CURRENT_URL] = "";
 	/**
 	* Constructor
-	* @param {String | Object} key The key of the state information to be stored <ko>저장할 상태 정보의 키</ko>
-	* @param {String} [key.key]  The key of the state information to be stored <ko>저장할 상태 정보의 키</ko>
-	* @param {Boolean} [key.excludeHash] This option can store or get information about url except hash. <ko>이 옵션은 hash를 제외한 url에 대한 정보를 저장화거나 가져올 수 있다.</ko>
+	* @param {String} key The key of the state information to be stored <ko>저장할 상태 정보의 키</ko>
 	**/
 	constructor(key) {
-		this.state = {
-			excludeHash: false,
-			key: "",
-		};
-
-		const state = this.state;
-		const keyType = typeof key;
-
-		if (keyType === "string") {
-			state.key = key;
-		} else if (keyType === "object") {
-			for (const name in key) {
-				state[name] = key[name];
-			}
-		}
+		this.key = key;
 	}
 
 	/**
@@ -63,10 +108,13 @@ class Persist {
 	 * @return {String|Number|Boolean|Object|Array}
 	 */
 	get(path) {
+		const url = getUrl();
+		const urlKey = getKey(url);
+
+		// update url for pushState, replaceState
+		updateDepth(url, TYPE_NAVIGATE);
 		// find path
-		const {key, excludeHash} = this.state;
-		const urlKey = getUrlKey(excludeHash);
-		const globalState =	StorageManager.getStateByKey(urlKey, key);
+		const globalState =	StorageManager.getStateByKey(urlKey, this.key);
 
 		if (!path || path.length === 0) {
 			return globalState;
@@ -95,9 +143,14 @@ class Persist {
 	 * @return {Persist}
 	 */
 	set(path, value) {
+		const url = getUrl();
+		const urlKey = getKey(url);
+
+		// update url for pushState, replaceState
+		updateDepth(url, TYPE_NAVIGATE);
+
 		// find path
-		const {key, excludeHash} = this.state;
-		const urlKey = getUrlKey(excludeHash);
+		const key = this.key;
 		const globalState =	StorageManager.getStateByKey(urlKey, key);
 
 		if (path.length === 0) {
@@ -110,6 +163,8 @@ class Persist {
 			);
 		}
 
+		// If you are using set method, add the current url to depth.
+		addDepth(url);
 		return this;
 	}
 
@@ -123,4 +178,6 @@ class Persist {
 	}
 }
 
+// If navigation's type is not TYPE_BACK_FORWARD, delete information about current url.
+updateDepth(getUrl(), getNavigationType());
 export default Persist;
