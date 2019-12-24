10103efd5895d4bb74c4ac1318ed9127cd3b6f56
YOUN KUE CHOI <daybrush@gmail.com>
2019-12-24 13:26:22 +0900

    fix: add defense code in no cookie mode

--- src/browser.js
+++ src/browser.js
@@ -4,9 +4,21 @@ export {win as window};
 export const console = win.console;
 export const document = win.document;
 export const history = win.history;
-export const localStorage = win.localStorage;
 export const location = win.location;
-export const sessionStorage = win.sessionStorage;
 export const navigator = win.navigator;
 export const parseFloat = win.parseFloat;
 export const performance = win.performance;
+
+
+let localStorage;
+let sessionStorage;
+
+try {
+	localStorage = win.localStorage;
+	sessionStorage = win.sessionStorage;
+} catch (e) {
+	localStorage = null;
+	sessionStorage = null;
+}
+
+export { localStorage, sessionStorage };
