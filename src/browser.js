62add6d38ac2ea5671ab9538501c90b86fd236a7
Daybrush (Younkue Choi) <daybrush@gmail.com>
2020-01-03 15:46:57 +0900

    fix: add defense code in no cookie mode (#42)
    * fix: add defense code in no cookie mode
    
    * skip: apply review
    
    * skip: apply review

--- src/browser.js
+++ src/browser.js
@@ -4,9 +4,22 @@ export {win as window};
 export const console = win.console;
 export const document = win.document;
 export const history = win.history;
-export const localStorage = win.localStorage;
 export const location = win.location;
-export const sessionStorage = win.sessionStorage;
 export const navigator = win.navigator;
 export const parseFloat = win.parseFloat;
 export const performance = win.performance;
+
+let localStorage = null;
+
+let sessionStorage = null;
+
+try {
+	localStorage = win.localStorage;
+	sessionStorage = win.sessionStorage;
+} catch (e) {
+}
+
+export {
+	localStorage,
+	sessionStorage,
+};
